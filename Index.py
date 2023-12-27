9fae1f756294f4b8dc7b5524005e5fdf14756334
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2023-12-27 10:33:12 -0500

    v0

--- Index.py
+++ Index.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 import json
-import struct
 import sys
 import os
 import tarfile
@@ -21,11 +19,6 @@ def build_index(data_path):
     os.makedirs(DOCUMENTS_PATH, exist_ok=True)
 
     internal_id = 1
-    token_id = 1
-
-    lexicon = {}
-    inverted_index = defaultdict(list)
-    total_doc_length = 0
     
     # Process each file in the tar archive
     with tarfile.open(data_path, 'r:gz') as tar:
@@ -41,42 +34,19 @@ def build_index(data_path):
                             categories = extract_categories(content)
                             title = extract_title(content)
                             metadata_file_path, document_file_path = get_file_paths(internal_id)
-                            filtered_content = get_filtered_content(content)
-                            tokens = tokenize_text(filtered_content, remove_stop_words=True)
-                            total_doc_length += len(tokens) 
 
-                            # Count word frequency
-                            word_counts = defaultdict(int)
-                            for token in tokens:
-                                if not (token in lexicon):
-                                    token_id += 1
-                                    lexicon[token] = token_id
-                                word_counts[token_id] += 1
-
-                            # Update inverted index
-                            for token_id, freq in word_counts.items():
-                                inverted_index[str(token_id)].extend([internal_id, freq])
+                            # Make folders if they do not exist
+                            os.makedirs(os.path.dirname(metadata_file_path), exist_ok=True)
+                            os.makedirs(os.path.dirname(document_file_path), exist_ok=True)
 
                             # Save metadata and document content
                             with open(metadata_file_path, 'w+') as mf:
-                                json.dump({'internal_id': internal_id, 'title': title, 'categories': categories, 'doc_length': len(tokens)}, mf, indent=2)
+                                json.dump({'internal_id': internal_id, 'title': title, 'categories': categories}, mf, indent=2)
                             with open(document_file_path, 'w+') as df:
                                 df.write(content)
 
                             file.close()
                             internal_id += 1
-
-    # Print statistics
-    print("Total doc length: {}, Total docs: {}, Average doc length: {}".format(total_doc_length, internal_id - 1, total_doc_length / (internal_id - 1)))
-
-    # Save lexicon and inverted index
-    with open(LEXICON_PATH, "w+") as f:
-        json.dump(lexicon, f, indent=2)
-    with open(INVERTED_LEXICON_PATH, "w+") as f:
-        lexicon_inverted = {str(token_id): token for token, token_id in lexicon.items()}
-        json.dump(lexicon_inverted, f, indent=2)
-    with open(INVERTED_INDEX_PATH, 'w+') as f:
-        json.dump(inverted_index, f, indent=2)
 
 def main():
     # Check for command line arguments
@@ -85,7 +55,7 @@ def main():
         sys.exit(1)
 
     data_path = sys.argv[1]
-    build_index(data_path=data_path)
+    build_index(data_path)
 
 if __name__ == "__main__":
     main()
